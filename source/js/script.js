45128ae929072c0b183c101fe12bd5d6f92ef146
Qudenaza <qudenaza@gmail.com>
2021-03-24 15:31:29 +0300

    Adds intro section

--- source/js/script.js
+++ source/js/script.js
@@ -10,3 +10,29 @@ toggle.addEventListener('click', function () {
   nav.classList.toggle('page-nav--open')
   nav.classList.toggle('page-nav--closed')
 });
+
+// ФИКСИРОВАННОЕ МЕНЮ 
+
+
+
+// let main = document.querySelector(".page__main"),
+//  header = document.querySelector(".page__header");
+
+// function onScroll() {
+//   window.addEventListener("scroll", callbackFunc);
+
+//   function callbackFunc() {
+//     const y = window.pageYOffset;
+
+//     if (y > 0) {
+//       header.classList.add("header__fix");
+//     } else {
+//       header.classList.remove("header__fix");
+//     }
+//   }
+// }
+
+// window.onload = function() {
+//   onScroll();
+// };
+ 
