78b2c1203b4769faa8f99381e7f6e6e21ecb3c32
Qudenaza <qudenaza@gmail.com>
2021-04-02 17:06:23 +0300

    Fixed for catalog page on mobile version

--- source/js/script.js
+++ source/js/script.js
@@ -4,34 +4,32 @@ const toggle = document.querySelector('.toggle');
 
 // nav.classList.remove('page-nav--nojs');
 
-// toggle.addEventListener('click', function () {
-//   toggle.classList.toggle('toggle--active');
+toggle.addEventListener('click', function () {
+  toggle.classList.toggle('toggle--active');
 
-//   header.classList.toggle('header--open');
-// });
+  header.classList.toggle('header--open');
+});
 
 
 // ФИКСИРОВАННОЕ МЕНЮ
 
-// let introSection = document.querySelector(".intro");
+function onScroll() {
+  window.addEventListener("scroll", callbackFunc);
 
-// function onScroll() {
-//   window.addEventListener("scroll", callbackFunc);
+  function callbackFunc() {
+    const y = window.pageYOffset;
 
-//   function callbackFunc() {
-//     const y = window.pageYOffset;
+    if (y > 400) {
+      header.classList.add("header--fix");
+    } else {
+      header.classList.remove("header--fix");
+    }
+  }
+};
 
-//     if (y > introSection.offsetHeight) {
-//       header.classList.add("header--fix");
-//     } else {
-//       header.classList.remove("header--fix");
-//     }
-//   }
-// };
-
-// window.onload = function() {
-//   onScroll();
-// };
+window.onload = function() {
+  onScroll();
+};
 
 // МОДАЛКИ
 
